3512321d37d920c868be3b0c6d73b2fb42a173de
Spencer Mitchell <smitchell556@gmail.com>
2024-09-01 08:02:46 -0400

    refactor: fix up packaging and conda env export

--- conda_env_yml.py
+++ conda_env_yml.py
@@ -3,16 +3,33 @@ import tempfile
 
 import yaml
 
-p = subprocess.run('conda env export --from-history'.split(), capture_output=True)
+p = subprocess.run("conda env export --from-history".split(), capture_output=True)
 from_history = yaml.safe_load(p.stdout)
-dependencies = ['python'] + from_history['dependencies']
-for x in ['ca-certificates', 'certifi', 'openssl']:
+dependencies = ["python"] + from_history["dependencies"]
+for x in ["ca-certificates", "certifi", "openssl"]:
     dependencies.remove(x)
 
-p = subprocess.run('conda env export --no-builds'.split(), capture_output=True)
+p = subprocess.run("conda env export --no-builds".split(), capture_output=True)
 no_builds = yaml.safe_load(p.stdout)
-no_builds['dependencies'] = [x for x in no_builds['dependencies'] if x.split('=')[0] in dependencies]
-del no_builds['prefix']
 
-with open('environment.yml', 'w') as f:
+
+def extract_no_build_deps(deps):
+    rv = []
+    for dep in deps:
+        if isinstance(dep, dict):
+            # Skip pip dependencies.
+            continue
+        else:
+            if dep.split("=")[0] in dependencies:
+                rv.append(dep)
+
+    rv.append({"pip": ["git+https://github.com/spenceforce/NLP-Simple-to-Spectacular"]})
+
+    return rv
+
+
+no_builds["dependencies"] = extract_no_build_deps(no_builds["dependencies"])
+del no_builds["prefix"]
+
+with open("environment.yml", "w") as f:
     yaml.dump(no_builds, f)
