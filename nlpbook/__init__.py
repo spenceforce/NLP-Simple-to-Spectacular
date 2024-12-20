c43a9aa8e5ce7d505918156edee3a10b3e57532b
Spencer Mitchell <smitchell556@gmail.com>
2024-12-20 07:16:17 -0500

    feat: add model results dictionary for model result references

--- nlpbook/__init__.py
+++ nlpbook/__init__.py
@@ -1,3 +1,4 @@
 from .data_accessors import get_train_test_data, get_unsup_data
+from ._results import model_results
 
-__all__ = ["get_train_test_data", "get_unsup_data"]
+__all__ = ["get_train_test_data", "get_unsup_data", "model_results"]
---
+++ nlpbook/_results.py
@@ -0,0 +1,4 @@
+model_results = {
+    "baseline": 0.5011190233977619
+}
+# Results from models are copied here for easy access and comparison.
