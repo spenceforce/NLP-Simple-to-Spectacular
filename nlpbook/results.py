7f3fb5065719158fa82750173c8a108804ff4919
Spencer Mitchell <smitchell556@gmail.com>
2024-12-29 07:29:13 -0500

    fix: set model name as index

--- nlpbook/results.py
+++ nlpbook/results.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 # Results from models are copied here for easy access and comparison.
 model_results = [("Baseline", 0.5011190233977619), ("OneR (boc)", 0.5812817904374364)]
-model_results = pd.DataFrame([{"Model": m, "Accuracy": a} for m, a in model_results])
+model_results = pd.DataFrame([{"Model": m, "Accuracy": a} for m, a in model_results]).set_index("Model")
 
 
 def get_results(models=None):
     if models is None:
         return model_results
     return (
-        model_results[model_results["Model"].isin(models)].copy().reset_index(drop=True)
+        model_results[model_results.index.isin(models)].copy()
     )
