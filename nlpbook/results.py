8c7113f9a6adbff7bf8cc111d6c889c7289ee330
Spencer Mitchell <smitchell556@gmail.com>
2025-01-08 20:23:14 -0500

    chore: code formatting

--- nlpbook/results.py
+++ nlpbook/results.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 # Results from models are copied here for easy access and comparison.
 model_results = [("Baseline", 0.5011190233977619), ("OneR (boc)", 0.5812817904374364)]
-model_results = pd.DataFrame([{"Model": m, "Accuracy": a} for m, a in model_results]).set_index("Model")
+model_results = pd.DataFrame(
+    [{"Model": m, "Accuracy": a} for m, a in model_results]
+).set_index("Model")
 
 
 def get_results(models=None):
     if models is None:
         return model_results
-    return (
-        model_results[model_results.index.isin(models)].copy()
-    )
+    return model_results[model_results.index.isin(models)].copy()
