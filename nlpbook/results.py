4fb13b71ac60d58821759388e2ed101256080bcc
Spencer Mitchell <smitchell556@gmail.com>
2025-01-11 13:09:25 -0500

    feat: add results

--- nlpbook/results.py
+++ nlpbook/results.py
@@ -6,8 +6,20 @@ model_results = pd.DataFrame(
     [{"Model": m, "Accuracy": a} for m, a in model_results]
 ).set_index("Model")
 
+# Multiclass classification results.
+mc_model_results = [("Baseline", 0.1997151576805697), ("OneR (boc)", pd.NA)]
+mc_model_results = pd.DataFrame(
+    [{"Model": m, "Accuracy": a} for m, a in mc_model_results]
+).set_index("Model")
+
 
 def get_results(models=None):
     if models is None:
-        return model_results
+        return model_results.copy()
     return model_results[model_results.index.isin(models)].copy()
+
+
+def get_multiclass_results(models=None):
+    if models is None:
+        return mc_model_results.copy()
+    return mc_model_results[mc_model_results.index.isin(models)].copy()
