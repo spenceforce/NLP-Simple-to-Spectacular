7ad10332c8cdbec9e90e61a96403030b713f4acf
Spencer Mitchell <smitchell556@gmail.com>
2025-01-11 13:38:09 -0500

    chore: code formatting

--- nlpbook/__init__.py
+++ nlpbook/__init__.py
@@ -1,4 +1,10 @@
 from .data_accessors import get_train_test_data, get_unsup_data
 from .results import model_results, get_results, get_multiclass_results
 
-__all__ = ["get_train_test_data", "get_unsup_data", "model_results", "get_results", "get_multiclass_results"]
+__all__ = [
+    "get_train_test_data",
+    "get_unsup_data",
+    "model_results",
+    "get_results",
+    "get_multiclass_results",
+]
