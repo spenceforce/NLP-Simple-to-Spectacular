67d582dac7d48cd9bdf30ea61b37464b6409041c
Spencer Mitchell <smitchell556@gmail.com>
2025-01-28 06:35:47 -0500

    feat: remove multiclass classification

--- nlpbook/results.py
+++ nlpbook/results.py
@@ -10,24 +10,8 @@ model_results = pd.DataFrame(
     [{"Model": m, "Accuracy": a} for m, a in model_results]
 ).set_index("Model")
 
-# Multiclass classification results.
-mc_model_results = [
-    ("Baseline", 0.1997151576805697),
-    ("OneR (length)", 0.17367243133265514),
-    ("OneR (boc)", 0.19377416073245168),
-]
-mc_model_results = pd.DataFrame(
-    [{"Model": m, "Accuracy": a} for m, a in mc_model_results]
-).set_index("Model")
-
 
 def get_results(models=None):
     if models is None:
         return model_results.copy()
     return model_results[model_results.index.isin(models)].copy()
-
-
-def get_multiclass_results(models=None):
-    if models is None:
-        return mc_model_results.copy()
-    return mc_model_results[mc_model_results.index.isin(models)].copy()
