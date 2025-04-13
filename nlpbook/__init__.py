cf2f4843ced29eb549fbe193c909387d95d9397d
Spencer Mitchell <smitchell556@gmail.com>
2025-04-13 18:48:40 -0400

    fixup! fix: remove missing function

--- nlpbook/__init__.py
+++ nlpbook/__init__.py
@@ -1,5 +1,5 @@
 from .data_accessors import get_train_test_data, get_unsup_data
-from .results import model_results, get_results, get_multiclass_results
+from .results import model_results, get_results
 
 __all__ = [
     "get_train_test_data",
