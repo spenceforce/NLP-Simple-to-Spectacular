7c7f11d26b0382d3befaac52f6db5ad0240ad20b
Spencer Mitchell <smitchell556@gmail.com>
2025-04-27 13:49:07 -0400

    feat(tokenizer): enhance tokenizer functionality and update API
    - Renamed ~fit~ method to ~train~ for better clarity.
    - Added ~encode~ and ~decode~ methods for single input processing.
    - Introduced ~encode_batch~ and ~decode_batch~ methods for batch processing.
    - Updated special token handling to include square brackets as an option.
    - Improved code readability and structure in ~CharTokenizer~ class.
    
    BREAKING CHANGE: The ~fit~ method has been renamed to ~train~, and the ~encode~ and ~decode~ methods now handle single inputs instead of batches.

--- nlpbook/preprocessing/tokenizer.py
+++ nlpbook/preprocessing/tokenizer.py
@@ -1,7 +1,7 @@
 class CharTokenizer:
     """Encode and decode text."""
 
-    def fit(self, X):
+    def train(self, X):
         """Create a vocabulary from `X`."""
         vocabulary = set()
         for x in X:
@@ -10,7 +10,11 @@ class CharTokenizer:
         self.unk_token = "<unk>"
         self.cls_token = "<cls>"
         self.eos_token = "<eos>"
-        self.special_tokens = [self.unk_token, self.cls_token, self.eos_token]
+        self.special_tokens = [
+            self.unk_token,
+            self.cls_token,
+            self.eos_token,
+        ]
         self.tokens.extend(self.special_tokens)
         self.tok2idx = {tok: i for i, tok in enumerate(self.tokens)}
         self.unk_idx = self.tok2idx[self.unk_token]
@@ -26,23 +30,24 @@ class CharTokenizer:
             self.eos_token,
         ]
 
-    def encode(self, X):
+    def encode(self, x):
+        """Encode `x`."""
+        return [self.tok2idx[tok] for tok in self.tokenize(x)]
+
+    def encode_batch(self, X):
         """Encode each `str` in `X`."""
         rv = []
         for x in X:
-            rv.append([self.tok2idx[tok] for tok in self.tokenize(x)])
+            rv.append(self.encode(x))
         return rv
 
-    def decode(self, X):
+    def decode(self, x):
+        """Decode `x`."""
+        return "".join([self.tokens[i] for i in x[1:-1]])
+
+    def decode_batch(self, X):
         """Decode each encoding in `X` to a `str`."""
         rv = []
         for x in X:
-            self.verify_encoding(x)
-            rv.append("".join([self.tokens[i] for i in x[1:-1]]))
+            rv.append(self.decode(x))
         return rv
-
-    def verify_encoding(self, x):
-        """Verify the encoding of `x`."""
-        # Check the start and end tokens are present.
-        assert x[0] == self.cls_idx
-        assert x[-1] == self.eos_idx
